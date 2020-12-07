55274f1dfef43fe932e00fa6abad88957f3eeca7
SukhinaAndrey <47207386+SukhinaAndrey@users.noreply.github.com>
2020-12-07 15:05:58 +0200

    Add files via upload

---
+++ Travel/DBItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Travel
+{
+    public class DBItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Message { get; set; }
+    }
+}
